56e073a44671e45b608de8cea4441981321bb253
Vitalii <kicker198686@gmail.com>
2021-05-17 21:45:38 +0300

    finish

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import App from './App';
+import { AuthContextProvider } from './context-store/auth-context';
+
+const app = (
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
